44c9df2561c231d36f5a53f2cc8f3167f60075cb
Cam Gabriel <camille.gabriel@bit-okutama.jp>
2018-12-12 00:08:43 +0900

    add tanuki movement towards tower and prevent enemy collision

---
+++ Assets/Script/towerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class towerScript : MonoBehaviour {
+
+    public int HP = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            HP--;
+            Debug.Log("HP is now " + HP);
+        }
+
+        if(HP <= 0){
+            Destroy(this.gameObject);
+        }
+    }
+}
