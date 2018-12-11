c4be15d33b39b401a4c05dd82b6093ee181e6ba1
Zuhairi Rahmad <zuhairi.bit@gmail.com>
2018-12-11 21:32:51 +0900

    initial commit

---
+++ Assets/Script/ScheneChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScheneChanger : MonoBehaviour {
+
+	public static int SceneNumber;
+
+	void Start(){
+		if(SceneNumber == 0){
+			StartCoroutine(ToMenu());
+		}
+	}
+
+	IEnumerator ToMenu(){
+		yield return new WaitForSeconds(5);
+		SceneNumber = 1;
+		SceneManager.LoadScene(1);
+	}
+}
